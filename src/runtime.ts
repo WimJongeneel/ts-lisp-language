29c7dbca6de0a4e3a6680289414cb7aef52692a0
WimJongeneel <wim.jongeneel1@gmail.com>
2021-09-08 07:52:20 +0200

    Add scopes to memory

--- src/runtime.ts
+++ src/runtime.ts
@@ -9,7 +9,21 @@ interface Lambda {
     params: any[]
 }
 
-const memory = new Map<string, any>()
+const memory = [new Map<string, any>()]
+
+const define = (name: string, value: any) => memory[0].set(name, value)
+
+const get = (name: string) => {
+    for(const scope of memory) {
+        if(scope.has(name)) return scope.get(name)
+    }
+}
+
+const set = (name: string, value: any) => {
+    for(const scope of memory) {
+        if(scope.has(name)) return scope.set(name, value)
+    }
+}
 
 const functions = new Map<string, (args: any[]) => any>([
     [ '+', a => a.reduce((r, c) => r + c) ],
@@ -26,10 +40,10 @@ const functions = new Map<string, (args: any[]) => any>([
     [ 'greater', a => a[0] > a[1] ? 1 : 0 ],
     [ 'smaller', a => a[0] < a[1] ? 1 : 0],
     [ 'equals', a => a[0] == a[1] ? 1 : 0],
-    [ 'define', a => memory.set(a[0], a[1]) ],
-    [ 'inc', a => memory.set(a[0], memory.get(a[0]) + 1) ],
-    [ 'dec', a => memory.set(a[0], memory.get(a[0]) - 1) ],
-    [ '$', a => memory.get(a[0]) ],
+    [ 'define', a => define(a[0], a[1]) ],
+    [ 'inc', a => set(a[0], get(a[0]) + 1) ],
+    [ 'dec', a => set(a[0], get(a[0]) - 1) ],
+    [ '$', a => get(a[0]) ],
     [ 'call', a => ({ func: a[0], args: a.splice(1), kind: FuncKind }) ],
     [ 'list', a => a ],
     [ 'get', a => a[0][a[1]] ],
@@ -39,14 +53,17 @@ const functions = new Map<string, (args: any[]) => any>([
     [ '++', a => a.reduce((r, c) => [...r, ...c], [])],
     [ 'lambda', a => {
         const body = a[a.length - 1]
-        const args = a.splice(0, -1).map(String)
+        const args = a.splice(0, a.length -1).map(String)
         return { kind: LambdaKind, body, args, params: [] }
     }],
     [ 'apply', a => {
         const lambda = { ...a[0], params: a[0].params.concat(a.splice(1)) }
         if(lambda.args.length == lambda.params.length) {
-            // (lambda.args as string[]).forEach((p, i) => memory.set(p, lambda.params[i]))
-            return reduce(lambda.body)
+            memory.unshift(new Map<string, any>())
+            lambda.args.forEach((p, i) => define(p, lambda.params[i]))
+            const res = reduce(lambda.body)
+            memory.shift()
+            return res
         }
         return lambda
     }]
